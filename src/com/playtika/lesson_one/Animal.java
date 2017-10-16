07d311e26c76d7d5aac5d32918a7962fc4e6d9e0
rfilipchak <rfilipchak@playtika.com>
2017-10-16 20:08:44 +0300

    Lesson One

---
+++ src/com/playtika/lesson_one/Animal.java
@@ -0,0 +1,22 @@
+package com.playtika.lesson_one;
+
+/**
+ * Created by rfilipchak on 9/18/2017.
+ */ abstract class Animal{
+        final int age;// алт +ентер подсказка для ошибок
+         String color;
+
+    Animal(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Animal{");
+        sb.append("age=").append(age);
+        sb.append(", color='").append(color).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+
+}
---
+++ src/com/playtika/lesson_one/Bear.java
@@ -0,0 +1,12 @@
+package com.playtika.lesson_one;
+
+/**
+ * Created by rfilipchak on 9/18/2017.
+ */
+    class Bear extends Animal {
+        int weight ;
+
+        Bear(int age) {
+            super(age);
+        }
+}
